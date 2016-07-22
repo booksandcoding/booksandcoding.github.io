7f889d8b807598a3ad0331479335fba4813a2687
George <george@wade.me>
2016-07-22 21:02:07 +0100

    Update multiply.rb

--- _plugins/multiply.rb
+++ _plugins/multiply.rb
@@ -1,6 +1,9 @@
-class multipy < Liquid::Tag
-
-def multiply (a,b)
-a * b
+module Jekyll
+  module multiply
+    def multiply(a, b)
+      a * b
+    end
+  end
 end
 
+Liquid::Template.register_filter(Jekyll::multiply)
