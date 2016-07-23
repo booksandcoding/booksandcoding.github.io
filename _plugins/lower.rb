59b4102fa2512f8bc648d8b485bdeb5ebb264894
George <george@wade.me>
2016-07-23 11:27:24 +0100

    Delete lower.rb

--- _plugins/lower.rb
+++
@@ -1,9 +0,0 @@
-module MephistoArticlePlug
-  HOST = 'http://www.benlog.org'
-
-  def link_to_delicious(article, text = nil)
-    url =  'http://del.icio.us/post?'
-    url += "url=#{HOST}#{article['url']}&title=#{article['title']}"
-    content_tag :a, text || 'del.icio.us', :href => url
-  end
-end
